dbbb8fde0f42b3a7e40b9842469bcaccb897bc2f
maximzom <86885085+maximzom@users.noreply.github.com>
2024-05-26 20:22:30 +0300

    Add files via upload

---
+++ ChessGameByZelinskyiMS/ChessUI/MainWindow.xaml.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using ChessLogic;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highlights = new Rectangle[8, 8];
+        private readonly Dictionary<positionChess, Move> moveCache = new Dictionary<positionChess, Move>();
+
+        private gameStateChess gameState;
+        private positionChess selectedPos = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBoard();
+            gameState = new gameStateChess(playerChess.White, boardChess.Initial());
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.currentPlayer);
+        }
+
+        private void InitializeBoard()
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Image image = new Image();
+                    pieceImages[r, c] = image;
+                    PieceGrid.Children.Add(image);
+
+                    Rectangle highlight = new Rectangle();
+                    highlights[r, c] = highlight;
+                    HighlightGrid.Children.Add(highlight);
+                }
+            }
+        }
+
+        private void DrawBoard(boardChess board)
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    pieceChess piece = board[r, c];
+                    pieceImages[r, c].Source = Images.GetImage(piece);
+                }
+            }
+        }
+
+        private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (IsMenuOnScreen())
+            {
+                return;
+            }
+
+            Point point = e.GetPosition(BoardGrid);
+            positionChess pos = ToSquarePosition(point);
+
+            if (selectedPos == null)
+            {
+                OnFromPositionSelected(pos);
+            }
+            else
+            {
+                OnToPositionSelected(pos);
+            }
+        }
+
+        private positionChess ToSquarePosition(Point point)
+        {
+            double squareSize = BoardGrid.ActualWidth / 8;
+            int row = (int)(point.Y / squareSize);
+            int col = (int)(point.X / squareSize);
+            return new positionChess(row, col);
+        }
+
+        private void OnFromPositionSelected(positionChess pos)
+        {
+            IEnumerable<Move> moves = gameState.legalMovesForPiece(pos);
+
+            if (moves.Any())
+            {
+                selectedPos = pos;
+                CacheMoves(moves);
+                ShowHighlights();
+            }
+        }
+
+        private void OnToPositionSelected(positionChess pos)
+        {
+            selectedPos = null;
+            HideHighlights();
+
+            if (moveCache.TryGetValue(pos, out Move move))
+            {
+                if (move.Type == moveType.PawnPromotion)
+                {
+                    HandlePromotion(move.FromPos, move.ToPos);
+                }
+                else
+                {
+                    HandleMove(move);
+                }
+            }
+        }
+
+        private void HandlePromotion(positionChess from, positionChess to)
+        {
+            pieceImages[to.Row, to.Column].Source = Images.GetImage(gameState.currentPlayer, pieceType.Pawn);
+            pieceImages[from.Row, from.Column].Source = null;
+
+            PromotionMenu promMenu = new PromotionMenu(gameState.currentPlayer);
+            MenuContainer.Content = promMenu;
+
+            promMenu.PieceSelected += type =>
+            {
+                MenuContainer.Content = null;
+                Move promMove = new PawnPromotion(from, to, type);
+                HandleMove(promMove);
+            };
+        }
+
+        private void HandleMove(Move move)
+        {
+            gameState.makeMove(move);
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.currentPlayer);
+
+            if (gameState.IsGameOver())
+            {
+                ShowGameOver();
+            }
+        }
+
+        private void CacheMoves(IEnumerable<Move> moves)
+        {
+            moveCache.Clear();
+
+            foreach (Move move in moves)
+            {
+                moveCache[move.ToPos] = move;
+            }
+        }
+
+        private void ShowHighlights()
+        {
+            Color color = Color.FromArgb(150, 125, 255, 125);
+
+            foreach (positionChess to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = new SolidColorBrush(color);
+            }
+        }
+
+        private void HideHighlights()
+        {
+            foreach (positionChess to in moveCache.Keys)
+            {
+                highlights[to.Row, to.Column].Fill = Brushes.Transparent;
+            }
+        }
+
+        private void SetCursor(playerChess player)
+        {
+            if (player == playerChess.White)
+            {
+                Cursor = ChessCursors.WhiteCursor;
+            }
+            else
+            {
+                Cursor = ChessCursors.BlackCursor;
+            }
+        }
+
+        private bool IsMenuOnScreen()
+        {
+            return MenuContainer.Content != null;
+        }
+
+        private void ShowGameOver()
+        {
+            GameOverMenu gameOverMenu = new GameOverMenu(gameState);
+            MenuContainer.Content = gameOverMenu;
+
+            gameOverMenu.OptionSelected += option =>
+            {
+                if (option == Option.Restart)
+                {
+                    MenuContainer.Content = null;
+                    RestartGame();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            };
+        }
+
+        private void RestartGame()
+        {
+            selectedPos = null;
+            HideHighlights();
+            moveCache.Clear();
+            gameState = new gameStateChess(playerChess.White, boardChess.Initial());
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.currentPlayer);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!IsMenuOnScreen() && e.Key == Key.Escape)
+            {
+                ShowPauseMenu();
+            }
+        }
+
+        private void ShowPauseMenu()
+        {
+            PauseMenu pauseMenu = new PauseMenu();
+            MenuContainer.Content = pauseMenu;
+
+            pauseMenu.OptionSelected += option =>
+            {
+                MenuContainer.Content = null;
+
+                if (option == Option.Restart)
+                {
+                    RestartGame();
+                }
+            };
+        }
+    }
+}
---
+++ ChessGameByZelinskyiMS/ChessUI/PromotionMenu.xaml.cs
@@ -0,0 +1,45 @@
+﻿using ChessLogic;
+using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Interaction logic for PromotionMenu.xaml
+    /// </summary>
+    public partial class PromotionMenu : UserControl
+    {
+        public event Action<pieceType> PieceSelected;
+
+        public PromotionMenu(playerChess player)
+        {
+            InitializeComponent();
+
+            QueenImg.Source = Images.GetImage(player, pieceType.Queen);
+            BishopImg.Source = Images.GetImage(player, pieceType.Bishop);
+            RookImg.Source = Images.GetImage(player, pieceType.Rook);
+            KnightImg.Source = Images.GetImage(player, pieceType.Knight);
+        }
+
+        private void QueenImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(pieceType.Queen);
+        }
+
+        private void BishopImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(pieceType.Bishop);
+        }
+
+        private void RookImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(pieceType.Rook);
+        }
+
+        private void KnightImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(pieceType.Knight);
+        }
+    }
+}
